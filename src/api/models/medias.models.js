7dcaab42370fde6d95592da6b343c1efe3f02123
gonquintana <gonzalo.quintana@bootcamp-upgrade.com>
2022-11-27 11:53:49 +0100

    añadido medias

---
+++ src/api/models/medias.models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose') //Requerimos primero mongoose.
+
+const Schema = mongoose.Schema
+
+const mediasSchema = new Schema({
+
+    nameONDAS : {type : String},
+    nameREVISTA : {type : String},
+    image : {type : String, required: true}
+
+},{
+    timestamps : true
+})
+
+const Medias = mongoose.model('medias', mediasSchema ); //Aquí creamos nuestro modelo de series.
+
+module.exports = Medias; //Para exportar el modelo.
